d45b1721c40bfff9b5cac031a463be907f73060c
Ben Martin <ben@sam.localdomain>
2010-12-08 11:27:54 +1000

    Starting a separate repo for the testing code.

---
+++ para-add-delete/para-add-delete-odt-bold-embold-pipeend.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -I/usr/local/src/cvs/abiword/odf-2010-track-changes/test/odtct
+
+#
+# A little filter to trim out parts and highlight others 
+# to help with verification of ODT+CT files.
+#
+# This script is for M2 which is 
+#   insertion of paragraphs
+#   removal   of paragraphs
+#
+# Usage:
+#    ./para-add-delete-odt-bold-embold-pipeend.pl < /tmp/odt/o/content.xml
+# For HTML:
+#    ./para-add-delete-odt-bold-embold-pipeend.pl --output-html < /tmp/odt/o/content.xml
+#
+use IO::All;
+use odtct;
+
+my $io = io("| xmllint --format - |");
+
+odtct::setup();
+print $preamble;
+
+while( $line = $io->getline )
+{
+    next if $line =~ /office:document-content/;
+    $line = quoteLine( $line );
+
+    $line =~ s!delta:(removed-content[^>]*)!delta:$BoldOn$1$BoldOff!g;
+    $line =~ s!/delta:(removed-content)!/delta:$BoldOn$1$BoldOff!g;
+    $line =~ s!(change-idref="[^"]*")!$BoldOn$1$BoldOff!g;
+    $line =~ s!(insert-with-content)!$BoldOn$1$BoldOff!g;
+
+    $line = ColourLine( $line );
+    print $line;
+}
+
+print $postamble;
